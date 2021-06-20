2e40c4dbef65b1511dec58bbbb4e02f26934bd8f
alexionita95 <ionita.alexandru95@gmail.com>
2021-06-20 19:39:36 +0300

    Changed port to heroku env variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port =env.PORT || 3000
+const port =process.env.PORT || 3000
 var collection;
 app.get('/', (req, res) => {
     var data = "";
